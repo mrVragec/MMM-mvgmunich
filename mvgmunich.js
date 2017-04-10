c4ad8bc4e8ea67e52d920e9f7d18a2a9a761642f
Simon Crnko <simon.crnko@gmail.com>
2017-04-10 21:31:58 +0200

    - init commit

---
+++ mvgmunich.js
@@ -0,0 +1,59 @@
+/* Timetable for public transport in Munich */
+
+/*
+* Magic Mirror
+* Module: MVG Munich
+*
+* By Simon Crnko
+* MIT Licensed
+*
+*/
+
+Module.register("mvgmunich", {
+    // Default module configuration
+    defaults: {
+        maxEntries: 8, // maximum number of results shown on UI
+        updateInterval: 1 * 1000 * 30, // update every 30 seconds
+        //retryDelay: 5000,
+        fade: true,
+        fadePoint: 0.25,
+        apiBase: "http://www.mvg-live.de/ims/dfiStaticAnzeige.svc?",
+        //haltestelle: "Hauptbahnhof", // detault departure station
+        showUbahn: true, //show ubahn route
+        showBus: true, // show bus route
+        showTram: true, // show tram route
+        showSbahn: true, // show sbahn route
+    },
+
+    getStyles: function() {
+        return ["mvgmunich.css"];
+    },
+
+    start: function() {
+		Log.info("Starting module: " + this.name);
+        this.responseData = this.translate("LOADING");
+		this.sendSocketNotification("GETDATA", this.config);
+        this.responseData = "";
+        this.updateTimer = null;
+	},
+
+    // Override dom generator.
+	getDom: function() {
+        var wrapperTable = document.createElement("table");
+        wrapperTable.className = "small";
+        wrapperTable.innerHTML = this.responseData;
+		return wrapperTable;
+	},
+
+    // Override getHeader method.
+	getHeader: function() {
+		return this.data.header + " Munich: " + this.config.haltestelle;
+	},
+
+    socketNotificationReceived: function(notification, payload) {
+		if (notification === "UPDATE"){
+			this.responseData = payload;
+			this.updateDom();
+		}
+	}
+});
---
+++ node_helper.js
@@ -0,0 +1,91 @@
+/* Timetable for public transport in Munich */
+
+/*
+* Magic Mirror
+* Module: MVG Munich
+*
+* By Simon Crnko
+* MIT Licensed
+*
+*/
+
+var NodeHelper = require("node_helper");
+
+var cheerio = require('cheerio');
+var filter = require('array-filter');
+
+module.exports = NodeHelper.create({
+
+    start: function() {
+        this.updating = false;
+        this.started = false;
+        this.config = null;
+    },
+
+    /* updateTimetable(transports)
+    * Calls processTrains on succesfull response.
+    */
+    getData: function() {
+        var self = this;
+        var haltestelle = "haltestelle=" + this.config.haltestelle;
+        var ubahn = ((this.config.showUbahn) ? "&ubahn=checked" : "");
+        var bus = ((this.config.showBus) ? "&bus=checked" : "");
+        var tram = ((this.config.showTram) ? "&tram=checked" : "");
+        var sbahn = ((this.config.showSbahn) ? "&sbahn=checked" : "");
+		var urlApi = self.config.apiBase + haltestelle + ubahn + bus + tram + sbahn;
+		var retry = true;
+		var request = require('request');
+        request(urlApi, {encoding: 'binary'}, function (error, response, body) {
+            if(response.statusCode == 200 && !error) {
+                self.processData(body);
+            }
+        });
+    },
+
+    processData: function(data) {
+        var self = this;
+        this.dataRequest = data;
+        $ = cheerio.load(this.dataRequest);
+        var transport = "";
+        $('tr').each(function(i, elem) {
+            if($(this).html().includes('lineColumn')) {
+                transport += "<tr class='normal'>";
+                $(this).each(function(j, element) {
+                    transport += $(this).html().trim();
+                })
+                transport += "</tr>";
+            }
+            if(i >= self.config.maxEntries) {
+                return false;
+            }
+        });
+        this.sendSocketNotification("UPDATE", transport);
+    },
+
+    /* scheduleUpdate()
+    * Schedule next update.
+	* argument delay number - Milliseconds before next update. If empty, this.config.updateInterval is used.
+	*/
+	scheduleUpdate: function(delay) {
+        var nextLoad = this.config.updateInterval;
+        if (typeof delay !== "undefined" && delay >= 0) {
+		  nextLoad = delay;
+        }
+        nextLoad = nextLoad;
+		var self = this;
+        setInterval(function() {
+            //self.updateDom(); // no speed defined, so it updates instantly.
+            self.getData();
+        }, nextLoad); //perform every 1000 milliseconds.
+    },
+
+    socketNotificationReceived: function(notification, payload) {
+        const self = this;
+        if (notification === "GETDATA") {
+            this.config = payload;
+		    this.updating = true;
+            self.getData();
+            self.scheduleUpdate(this.config.updateInterval);
+        };
+    }
+});
